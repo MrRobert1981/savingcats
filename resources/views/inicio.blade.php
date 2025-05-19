5e3b5e6ee6d12283b668f15305037b706fa8feda
MrRobert1981 <mrrobert.smartphone@gmail.com>
2025-05-19 16:30:38 +0200

    favicon corporativo y renombrado de página de inicio

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Saving Cats</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="p-4">
+
+    <div class="container">
+        <h1 class="text-primary">¡Hola desde Laravel con Bootstrap!</h1>
+        <button class="btn btn-success mt-3">Botón de prueba</button>
+    </div>
+
+</body>
+</html>
