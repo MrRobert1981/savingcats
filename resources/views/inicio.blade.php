79e10d6f4bb4236cb8b5b3a662fbb07dfc82b6a8
MrRobert1981 <mrrobert.smartphone@gmail.com>
2025-05-19 17:33:14 +0200

    Página de inicio con cabecera y barra de navegación

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -1,15 +1,35 @@
 <!DOCTYPE html>
-<html>
+<html lang="es">
+
 <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>Saving Cats</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite('resources/css/inicio.css')
 </head>
-<body class="p-4">
 
-    <div class="container">
-        <h1 class="text-primary">¡Hola desde Laravel con Bootstrap!</h1>
-        <button class="btn btn-success mt-3">Botón de prueba</button>
+<body>
+
+    <div class="sticky-header">
+        <div id="webHeader">
+            <div id="webSiteTitleContainer">
+                <h1 id="webSiteTitle">Saving cats</h1>
+                <h1 id="backgroundWebSiteTitle">Saving cats</h1>
+            </div>
+        </div>
+        <div id="divNav">
+            <nav>
+                <ul>
+                    <li><a href="#" id="1">Test 1</a></li>
+                    <li><a href="#" id="2">Test 2</a></li>
+                    <li><a href="#" id="3">Test 3</a></li>
+                </ul>
+            </nav>
+        </div>
     </div>
 
+
 </body>
+
 </html>
