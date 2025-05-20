c93c60cbbc92594f22060fa9a9b9233529defb0f
MrRobert1981 <mrrobert.smartphone@gmail.com>
2025-05-20 16:50:30 +0200

    Simulación publicaciones bootstrap

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,25 @@
 
 @section('content')
     @auth
-        <span class="text-sm text-gray-700">Hola, {{ Auth::user()->name }}</span>
+        <div style="height: 20px;">
+            <span class="text-sm text-gray-700">Hola, {{ Auth::user()->name }}</span>
+        </div>
     @endauth
-    <p>Este es el contenido que va entre la cabecera y el pie.</p>
+    @guest
+        <div style="height: 20px;">
+        </div>
+    @endguest
+    <div class="container my-4">
+        <div class="row">
+            @for ($i = 1; $i <= 14; $i++)
+                <div class="col-6 col-md-4 col-lg-3 col-xl-2 mb-4 d-flex flex-column align-items-center">
+                    <div class="ratio ratio-1x1 w-100">
+                        <img src="https://images.unsplash.com/photo-1703783049515-867d492baa81?q=80&w=2047&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                            alt="Imagen {{ $i }}" class="img-fluid rounded">
+                    </div>
+                    <button class="btn btn-primary mt-2 w-100">Botón {{ $i }}</button>
+                </div>
+            @endfor
+        </div>
+    </div>
 @endsection
