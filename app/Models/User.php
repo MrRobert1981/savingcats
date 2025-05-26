30f7065c1abd6a2a85fe5134b6f753db8ac7260e
MrRobert1981 <mrrobert.smartphone@gmail.com>
2025-05-26 13:10:54 +0200

    Método que devuelve todos los gatos propiedad de un usuario

--- app/Models/User.php
+++ app/Models/User.php
@@ -49,4 +49,9 @@ class User extends Authenticatable
     {
         return $this->role === 'admin';
     }
+
+    public function cats()
+    {
+        return $this->hasMany(Cat::class, 'owner_id');
+    }
 }
